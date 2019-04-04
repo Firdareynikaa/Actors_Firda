b6e7394d51d104d0f10a910ef0d443b9802fdf05
Firdareynikaa <firda_reinika_26rpl@student.smktelkom-mlg.sch.id>
2019-04-04 22:11:55 +0700

    selesai

---
+++ app/src/main/java/com/dycode/edu/actors/ActorsResponse.java
@@ -0,0 +1,20 @@
+package com.dycode.edu.actors;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ActorsResponse {
+
+    @SerializedName("actors")
+    private List<ActorsItem> actors;
+
+    public List<ActorsItem> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<ActorsItem> actors) {
+
+        this.actors = actors;
+    }
+}
